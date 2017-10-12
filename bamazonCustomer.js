76c86a9d993a8e28b1314bf363a1ffec9c3856d3
James Waite <James Waite>
2017-10-12 16:12:19 -0400

    added images folder

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: Password,
+    password: "FuggitLexiconBayblades2",
     database: "Bamazon"
 });
 
